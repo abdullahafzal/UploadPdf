4424869013522ad5b79291651a3e1e69b5844768
Abdullah <abdullahafzal2121@gmail.com>
2020-07-31 15:22:19 +0500

    uploading the project

---
+++ READNE.md
@@ -0,0 +1,1 @@
+"# create python envoriment by running this command python -m venv env ... Now run the command to activate it env\Scripts\activate.bat   ... now install all the packages pip install -r requirments.txt " 
---
+++ main.py
@@ -0,0 +1,85 @@
+import os
+# import magic
+import urllib.request
+from app import app
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+from bs4 import BeautifulSoup
+import codecs
+import pandas as pd
+import os
+import json
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/', methods=['POST','GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            f = codecs.open(filename, 'r', encoding="utf8")
+            html = f.read()
+            soup = BeautifulSoup(html, 'html.parser')
+            message_id = []
+            date_time = []
+            description = []
+            from_name = []
+            title = []
+
+            for message in soup.find_all('div', attrs={'class': 'message'}):
+                try:
+                    message_id.append(message['id'])
+                except:
+                    message_id.append('')
+                try:
+                    date_time.append(message.find('div', attrs={'class': 'pull_right date details'})['title'])
+                except:
+                    date_time.append('')
+
+                try:
+                    description.append(message.find('div', attrs={'class': 'description'}).text)
+                except:
+                    description.append('')
+
+                try:
+                    from_name.append(message.find('div', attrs={'class': 'from_name'}).text)
+                except:
+                    from_name.append('')
+
+                try:
+                    title.append(message.find('div', attrs={'class': 'title bold'}).text)
+                except:
+                    title.append('')
+
+            columns = ['message id', 'Date and Time', 'form name', 'title', 'description']
+
+            data = list(zip(message_id, date_time, from_name, title, description))
+            df = pd.DataFrame(data, columns=columns)
+            return df.to_json()
+            
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+
+
+if __name__ == "__main__":
+    app.run()
